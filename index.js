96e0fad508fbeca1f4e80be3ab08033fa61a9d16
Laura Gabriele <lauragabriele2015@gmail.com>
2023-03-02 14:49:51 -0300

    Criando uma To Do List

---
+++ index.js
@@ -0,0 +1,12 @@
+var itensLista=getElementById('itensLista')
+var itensAdicionados=getElementById('text') 
+
+function adicionar(){
+    
+        
+}
+
+
+function getElementById(id){
+    return document.getElementById(id)
+}
